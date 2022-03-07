3b679de2bc9547ff554ce221940bb249788355d1
karthik <karthiklaksh1729@gmail.com>
2022-03-07 23:31:49 +0530

    Modified rfetch.v and pc prediction

--- Pipeline/pc_predict.v
+++ Pipeline/pc_predict.v
@@ -1,29 +1,24 @@
-module pc_update(clk, icode, PC, valP, valM, valC, cnd, newPC);
+module pc_update(clk, icode, PC, valP, valM, valC, cnd, predPC);
 
     input clk, cnd;
     input [3:0] icode;
     input [63:0] valP, valM, valC, PC;
 
-    output reg [63:0] newPC;                                                            //Updated PC value
+    output reg [63:0] predPC;                                                            //Updated PC value
 
     always @(*) begin
         
         if (icode == 2 || icode == 3 || icode == 4 || icode == 5 || icode == 6 || icode == 10 || icode == 11) begin                                                                          //cmovXX, irmovq, rmmovq, mrmovq, OPq, pushq, popq
-            newPC = valP;
+            predPC = valP;
         end
         else if (icode == 7) begin                                                   //jXX
-            if (cnd == 1) begin
-                newPC = valC;
-            end
-            else begin
-                newPC = valP;
-            end
+            predPC = valC;
         end
         else if (icode == 8) begin                                                  //call
-            newPC = valC;
+            predPC = valC;
         end
         else if (icode == 9) begin                                                  //ret
-            newPC = valM;
+            predPC = valM;
         end
 
     end
