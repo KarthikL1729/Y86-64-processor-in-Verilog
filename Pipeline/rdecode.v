7ded408576142b2c4e6f8ba0f9e173e5ae841bb0
karthik <karthiklaksh1729@gmail.com>
2022-03-07 23:02:49 +0530

    Added decode and write back changes

--- Pipeline/rdecode.v
+++ Pipeline/rdecode.v
@@ -1,21 +1,21 @@
-module rdecode(clk, D_stat, D_icode, D_ifun ,D_rA, D_rB, D_valC, D_valP, d_stat, d_icode, d_ifun ,d_rA, d_rB, d_valC, d_valP);
+module rdecode(clk, f_stat, f_icode, f_ifun ,f_rA, f_rB, f_valC, f_valP, D_stat, D_icode, D_ifun ,D_rA, D_rB, D_valC, D_valP);
 
     input clk;
-    input [1:0] D_stat;
-    input [3:0] D_icode, D_ifun, D_rA, D_rB;
-    input [63:0] D_valC, D_valP;
-    output [1:0] d_stat;
-    output [3:0] d_icode, d_ifun, d_rA, d_rB;
-    output [63:0] d_valC, d_valP;
+    input [1:0] f_stat;
+    input [3:0] f_icode, f_ifun, f_rA, f_rB;
+    input [63:0] f_valC, f_valP;
+    output [1:0] D_stat;
+    output [3:0] D_icode, D_ifun, D_rA, D_rB;
+    output [63:0] D_valC, D_valP;
 
     always @(posedge clk) begin
-        d_stat <= D_stat;
-        d_icode <= D_icode;
-        d_ifun <= D_ifun;
-        d_rA <= D_rA;
-        d_rB <= D_rB;
-        d_valC <= D_valC;
-        d_valP <= D_valP;
+        D_stat <= f_stat;
+        D_icode <= f_icode;
+        D_ifun <= f_ifun;
+        D_rA <= f_rA;
+        D_rB <= f_rB;
+        D_valC <= f_valC;
+        D_valP <= f_valP;
     end
 
 endmodule
