0b6ffd283dfae9b1afaece32ec9ab17ef92d5f31
karthik <karthiklaksh1729@gmail.com>
2022-03-08 10:42:52 +0530

    Added PC select

--- Pipeline/pc_predict.v
+++ Pipeline/pc_predict.v
@@ -1,6 +1,5 @@
-module pc_update(clk, icode, PC, valP, valM, valC, cnd, predPC);
+module pc_predict(icode, PC, valP, valM, valC, predPC);
 
-    input clk, cnd;
     input [3:0] icode;
     input [63:0] valP, valM, valC, PC;
 
@@ -21,6 +20,25 @@ module pc_update(clk, icode, PC, valP, valM, valC, cnd, predPC);
             predPC = valM;
         end
 
+    end
+
+endmodule
+
+module pc_select(M_icode, M_Cnd, M_valA, W_icode, W_valM, F_predPC, f_PC);
+
+    input M_Cnd;
+    input [3:0] M_icode, W_icode;
+    input [63:0] M_valA, W_valM, F_predPC;
+    output reg[63:0] f_PC;
+
+    if(M_icode == 7 && !M_Cnd) begin                // jXX instruction
+        f_PC = M_valA;
+    end
+    else if (W_icode == 9) begin                    // ret instruction
+        f_PC = W_valM;
+    end
+    else begin
+      f_PC = F_predPC;
     end
 
 endmodule
