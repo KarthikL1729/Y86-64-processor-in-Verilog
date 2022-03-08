e3cdddbb93d6fb1170de1d18387de1deb564b60d
The-Endurance <ananya.sane@gmail.com>
2022-03-08 17:12:31 +0530

    everything but processor.v

--- Pipeline/memory.v
+++ Pipeline/memory.v
@@ -1,29 +1,67 @@
-module memory(clk, icode, valP, valA, valB, valE, valM);
+module memory(M_stat, M_icode, M_cnd, M_valE, M_valA, M_dstE, M_dstM, m_stat, m_icode, m_valE, m_valM, m_dstE, m_dstM, M_cndfwd, M_valAfwd, M_valEfwd)
 
-    input clk;
-    input [3:0] icode;
-    input [63:0] valA, valB, valE, valP;
+    input M_cnd;
+    input [3:0] M_stat;
+    input [3:0] M_icode M_dstE, M_dstM;
+    input [63:0] M_valA, M_valE;
 
-    output reg [63:0] valM;                                                         
+    output reg [63:0] m_valE, m_valM, M_valAfwd, M_valEfwd; 
+    output reg [3:0] m_dstE, m_dstM, m_icode;
+    output reg [3:0] m_stat;
+    output reg M_cndfwd;
+                                                        
     reg [63:0] datamem [2047:0];                                               //Data memory 2048*8 bytes
 
-    always @(*) begin
+    always @(M_stat, M_icode, M_cnd, M_valE, M_valA, M_dstE, M_dstM) begin
         
-        if (icode == 2 || icode == 3 || icode == 6 || icode == 7) begin           //cmovXXX, irmovq, OPq, jXX
+        m_icode = M_icode;
+        m_valE = M_valE;
+        m_dstE = M_dstE;
+        m_dstM = M_dstM;
+        M_valAfwd = M_valA;
+        M_valEfwd = M_valE;
+        M_cndfwd = M_cnd;
+        m_stat = M_stat;
+
+       
+            
+        if (M_icode == 2 || M_icode == 3 || M_icode == 6 || M_icode == 7) begin           //cmovXXX, irmovq, OPq, jXX
         end
-        else if (icode == 4 || icode == 10) begin                                 //rmmovq, pushq
-            datamem[valE] = valA;
+        else if (M_icode == 4 || M_icode == 10) begin   //rmmovq, pushq
+            if(M_valE>=0 && M_valE <=2047) begin
+                datamem[M_valE] = M_valA;
+            end    
+            else begin
+                m_stat[3] = 1;
+            end                            
         end
-        else if (icode == 5) begin                                                //mrmovq
-            valM = datamem[valE];
+        else if (M_icode == 5) begin                                                //mrmovq
+            if(M_valE>=0 && M_valE <=2047) begin
+                m_valM = datamem[M_valE];
+            end    
+            else begin
+                m_stat[3] = 1;
+            end
         end
-        else if (icode == 8) begin                                                //call                  
-            datamem[valE] = valP;
+        else if (M_icode == 8) begin                                                //call                  
+            if(M_valE>=0 && M_valE <=2047) begin
+                datamem[M_valE] = M_valA;
+            end    
+            else begin
+                m_stat[3] = 1;
+            end
         end
-        else if (icode == 9 || icode == 11) begin                                   //ret, popq          
-           valM = datamem[valA]; 
+        else if (M_icode == 9 || M_icode == 11) begin                                   //ret, popq          
+           if(M_valA>=0 && M_valA <=2047) begin
+                m_valM = datamem[M_valA]; 
+            end    
+            else begin
+                m_stat[3] = 1;
+            end
         end
 
     end
+
+    
 
 endmodule
--- Pipeline/write_back.v
+++ Pipeline/write_back.v
@@ -1,50 +1,18 @@
-module write_back(clk, cnd, icode, rA, rB, valM, valE, dstA, dstB, dataA, dataB);
+module write_back(W_stat, W_icode, W_valE, W_valM, W_dstE, W_dstM, w_stat, w_icode, w_valE, w_valM, w_dstE, w_dstM);
+    input [3:0] W_stat, W_icode, W_dstE, W_dstM;
+    input [63:0] W_valE, W_valM;
 
-    input clk, cnd;
-    input [3:0] icode, rA, rB;
-    input [63:0] valM, valE;
-    output reg [3:0] dstA, dstB;
-    output reg [63:0] dataA, dataB;
+    output reg [3:0] w_stat, w_icode, w_dstE, w_dstM;
+    output reg [63:0] w_valE, w_valM;
 
-   
-    always @(*) begin
-        
-        if (icode == 2 && cnd == 1) begin                               //cmovXXX 
-            //regArr[rB] = valE;
-            dstA = rB;
-            dataA = valE;
-            dstB = 15;
-        end
-        else if (icode == 3 || icode == 6) begin                        //irmovq, OPq
-            //regArr[rB] = valE;
-            dstA = rB;
-            dataA = valE;
-            dstB = 15;
-        end
-        else if (icode == 4 || icode == 7) begin                         //rmmovq, jXX
-        end
-        else if (icode == 5) begin                                       //mrmovq
-            //regArr[rA] = valM;
-            dstA = rA;
-            dataA = valM;
-            dstB = 15;
-        end
-        else if (icode == 8 || icode == 9 || icode == 10) begin          //call
-            //regArr[14] = valE;
-            dstA = 14;
-            dataA = valE;
-            dstB = 15;
-        end
-        else if (icode == 11) begin                                      //popq
-            //regArr[14] = valE;
-            //regArr[rA] = valM;
-
-            dstA = 14;
-            dataA = valE;
-
-            dstB = rA;
-            dataB = valM;
-        end
+    always @(W_stat, W_icode, W_valE, W_valM, W_dstE, W_dstM) begin
+        w_stat = W_stat;  
+        w_icode = W_icode;
+        w_valE = W_valE;
+        w_valM = W_valM;
+        w_dstE = W_dstE;
+        w_dstM = W_dstM;
     end
+   
 
 endmodule
