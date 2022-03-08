e13d3d15a88c633590d7612dbc5bc6882adcd9e6
The-Endurance <ananya.sane@gmail.com>
2022-03-08 15:19:21 +0530

    pipe decode stage done hopefully

--- Pipeline/decode.v
+++ Pipeline/decode.v
@@ -1,38 +1,134 @@
-module decode(clk, icode, valAin, valBin, stkPt, valAout, valBout);
+`include "regarr.v"
 
-    input clk;
-    input [3:0] icode;
-    input [63:0] valAin, valBin, stkPt;                           
+
+module decode(D_stat, D_icode, D_ifun, rA, rB, valC, valP, e_dstE, e_valE, M_dstE, M_valE, M_dstM, m_valM, W_dstM, W_valM, W_dstE, W_valE, d_stat, d_icode, d_ifun, d_valC, d_valA, d_valB, d_dstE, d_dstM, d_srcA, d_srcB);
+
+    input [2:0] D_stat;
+    input [3:0] D_icode, D_ifun;
+    input [3:0] rA, rB, e_dstE, M_dstE, M_dstM, W_dstM, W_dstE;  
+    input [63:0] valC, valP, e_valE, M_valE, m_valM, W_valM, W_valE;                         
     //8 byte values in the registers, stk is (%rsp)
-    output reg [63:0] valAout, valBout;                           // regArr[14] is stack pointer  
+    output [2:0] d_stat, d_icode, d_ifun;
+    output [63:0] d_valC, d_valA, d_valB;
+    output [3:0] d_dstE, d_dstM, d_srcA, d_srcB;                          // regArr[14] is stack pointer  
 
+    reg [63:0] d_rvalA, d_rvalB, valStk;
 
-    always @(icode, valAin, valBin, stkPt) begin
+    regarr regfile(.srcA(d_srcA), .srcB(d_srcB), .valA(d_rvalA), .valB(d_rvalB), .valStk(valStk), .dstM(W_dstM), .dstE(W_dstE), .M(W_valM), .E(W_valE));
+
+    always @(D_icode, D_ifun, rA, rB, valC, valP) begin
         
-        if (icode == 2) begin                               //cmovxx instruction encountered
-            valAout = valAin;
-            valBout = 0;
+        d_stat = D_stat;
+        d_icode = D_icode;
+        d_ifun = D_ifun;
+        d_valC = valC;
+
+        case (icode)
+            2: begin    //cmovxx
+                d_dstE = 15;
+                d_dstM = rB;
+                d_srcA = rA;
+                d_srcB = 15;
+            end
+            3: begin    //irmovq
+                d_dstE = rB;
+                d_dstM = 15;
+                d_srcA = 15;
+                d_srcB = 15;
+            end
+            4: begin    //rmmovq
+                d_dstE = rB;
+                d_dstM = 15;
+                d_srcA = rA;
+                d_srcB = rB;
+            end
+            5: begin    //mrmovq
+                d_dstE = 15;
+                d_dstM = rA;
+                d_srcA = 15;
+                d_srcB = rB;
+            end
+            6: begin    //opq
+                d_dstE = rB;
+                d_dstM = rA;
+                d_srcA = rA;
+                d_srcB = rB;
+            end
+            8: begin    //call  
+                d_dstE = 14;
+                d_dstM = 15;
+                d_srcA = 15;
+                d_srcB = 14;
+            end
+            9: begin    //ret
+                d_dstE = 14;
+                d_dstM = 15;
+                d_srcA = 14;
+                d_srcB = 14;
+            end
+            10: begin   //pushq
+                d_dstE = 14;
+                d_dstM = 15;
+                d_srcA = rA;
+                d_srcB = 14;
+            end
+            11: begin //popq
+                d_dstE = 14;
+                d_dstM = rA;
+                d_srcA = 14;
+                d_srcB = 14;
+            end
+            default: begin
+                d_dstE = 15;
+                d_dstM = 15;
+                d_srcA = 15;
+                d_srcB = 15;
+            end
+        endcase
+
+        d_valA = d_rvalA;
+
+        if(D_icode == 8 || D_icode == 7) begin
+            d_valA = valP;
         end
-        else if (icode == 3 || icode == 7) begin            //irmovq, jXX
+        if(d_srcA == e_dstE) begin
+            d_valA = e_valE;
         end
-        else if(icode == 4 || icode == 6) begin             //rmmovq, OPq
-            valAout = valAin;
-            valBout = valBin;
+        if(d_srcA == M_dstM) begin
+            d_valA = m_valM;
         end
-        else if(icode == 5) begin                           //mrmovq
-            valBout = valBin;
+        if(d_srcA == M_dstE) begin
+            d_valA = M_valE;
         end
-        else if(icode == 8) begin                           //call
-            valBout = stkPt;                                //stk
-        end          
-        else if (icode == 9 || icode == 11) begin           //ret, popq
-            valAout = stkPt;
-            valBout = stkPt;
+        if(d_srcA == W_dstM) begin
+            d_valA = W_valM;
         end
-        else if (icode == 10) begin                         //pushq
-            valAout = valAin;
-            valBout = stkPt;
+        if(d_srcA == W_dstE) begin
+            d_valA = W_valE;
         end
 
+        d_valB = d_rvalB;
+
+        if(d_srcB == e_dstE) begin
+            d_valB = e_valE;
+        end
+        if(d_srcB == M_dstM) begin
+            d_valB = m_valM;
+        end
+        if(d_srcB == M_dstE) begin
+            d_valB = M_valE;
+        end
+        if(d_srcB == W_dstM) begin
+            d_valB = W_valM;
+        end
+        if(d_srcB == W_dstE) begin
+            d_valB = W_valE;
+        end
+        
+
+
+
+        
     end
+
 endmodule
--- Pipeline/regarr.v
+++ Pipeline/regarr.v
@@ -1,7 +1,7 @@
-module regarr(PC, rA, rB, valA, valB, valStk, dstA, dstB, wrtA, wrtB);
-    input [63:0] PC;
-    input [3:0] rA, rB, dstA, dstB;
-    input [63:0] wrtA, wrtB;
+module regarr(srcA, srcB, valA, valB, valStk, dstM, dstE, M, E);
+    //input [63:0] PC;
+    input [3:0] srcA, srcB, dstM, dstE;
+    input [63:0] M, E;
     //output [63:0] reg0, reg3;
     //input rw;       //0 for read and 1 for write
 
@@ -9,21 +9,21 @@ module regarr(PC, rA, rB, valA, valB, valStk, dstA, dstB, wrtA, wrtB);
 
     reg [63:0] regArr[14:0];
 
-    always @(rA, rB, PC) begin
-        valA = regArr[rA];
-        valB = regArr[rB];
+    always @(srcA, srcB, PC) begin
+        valA = regArr[srcA];
+        valB = regArr[srcB];
         valStk = regArr[14];
     end
 
-    always @(dstA, wrtA, dstB, wrtB) begin
-        regArr[dstA] <= wrtA;
-        regArr[dstB] <= wrtB;
+    always @(dstM, M, dstE, E) begin
+        regArr[dstM] <= M;
+        regArr[dstE] <= E;
 
     end
 
-    always @(PC) begin
-        $display("0: %d\n1: %d\n2: %d\n3: %d\n4: %d\n5: %d\n6: %d\n7: %d\n8: %d\n9: %d\n10: %d\n11: %d\n12: %d\n13: %d\n14: %d\n",regArr[0],regArr[1],regArr[2],regArr[3],regArr[4],regArr[5],regArr[6],regArr[7],regArr[8],regArr[9],regArr[10],regArr[11],regArr[12],regArr[13],regArr[14]);
-    end
+    // always @(PC) begin
+    //     $display("0: %d\n1: %d\n2: %d\n3: %d\n4: %d\n5: %d\n6: %d\n7: %d\n8: %d\n9: %d\n10: %d\n11: %d\n12: %d\n13: %d\n14: %d\n",regArr[0],regArr[1],regArr[2],regArr[3],regArr[4],regArr[5],regArr[6],regArr[7],regArr[8],regArr[9],regArr[10],regArr[11],regArr[12],regArr[13],regArr[14]);
+    // end
 
     //assign reg0 = regArr[0];
     //assign reg3 = regArr[3];
